f862d30eb97500f58c0ce904bfb679fa05a24e5c
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-18 13:41:14 +0100

    Created project
    Created the Android Studio project

---
+++ app/src/main/java/com/heistejiri/weather_appandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.heistejiri.weather_appandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
